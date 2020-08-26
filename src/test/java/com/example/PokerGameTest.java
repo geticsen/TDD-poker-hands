c9b35e00b166a5d17fb4038c72912013dda48e22
ZHOUGR2 <286348794@qq.com>
2020-08-26 20:37:29 +0800

    [TDD practice] [christopher&green] feature: add should_return_black_win_when_play_poker_given_3H4C5D6H7C_5C6C7D8S9H test case

--- src/test/java/com/example/PokerGameTest.java
+++ src/test/java/com/example/PokerGameTest.java
@@ -8,7 +8,8 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PokerGameTest {
-    private  static PokerGame pokerGame;
+    private static PokerGame pokerGame;
+
     @BeforeAll
     public static void setUp() throws Exception {
         pokerGame = new PokerGame();
@@ -20,11 +21,11 @@ public class PokerGameTest {
         String whiteString = "3H 4H 5H 6H 7H";
         String blackString = "2H 8D 8C 8S 8H";
         String result;
-        String expect="White win";
+        String expect = "White win";
         //when
-        result = pokerGame.play(whiteString,blackString);
+        result = pokerGame.play(whiteString, blackString);
         //then
-        Assertions.assertEquals(expect,result);
+        Assertions.assertEquals(expect, result);
     }
 
     @Test
@@ -33,48 +34,63 @@ public class PokerGameTest {
         String whiteString = "3H 4D 5C 6S 8D";
         String blackString = "3H 4D 5C 6S 9D";
         String result;
-        String expect="Black win";
+        String expect = "Black win";
         //when
-        result = pokerGame.play(whiteString,blackString);
+        result = pokerGame.play(whiteString, blackString);
         //then
-        Assertions.assertEquals(expect,result);
+        Assertions.assertEquals(expect, result);
     }
 
     @Test
     void should_return_black_win_when_play_poker_given_5H5C5D5S2D_3H3C3D3S4C() {
         //given
-        String blackString  = "3H 3C 3D 3S 4C";
-        String whiteString  = "5H 5C 5D 5S 2D";
+        String blackString = "3H 3C 3D 3S 4C";
+        String whiteString = "5H 5C 5D 5S 2D";
         String result;
-        String expect="White win";
+        String expect = "White win";
         //when
-        result = pokerGame.play(whiteString,blackString);
+        result = pokerGame.play(whiteString, blackString);
         //then
-        Assertions.assertEquals(expect,result);
+        Assertions.assertEquals(expect, result);
     }
+
     @Test
     void should_return_white_win_when_play_poker_given_3H4H5H6H7H_and_5H6H7H8H9H() {
         //given
-        String blackString  = "3H 4H 5H 6H 7H";
-        String whiteString  = "5H 6H 7H 8H 9H";
+        String blackString = "3H 4H 5H 6H 7H";
+        String whiteString = "5H 6H 7H 8H 9H";
         String result;
-        String expect="White win";
+        String expect = "White win";
         //when
-        result = pokerGame.play(whiteString,blackString);
+        result = pokerGame.play(whiteString, blackString);
         //then
-        Assertions.assertEquals(expect,result);
+        Assertions.assertEquals(expect, result);
     }
+
     @Test
     void should_return_black_win_when_play_poker_given_2H3C4D6S7C_5S6C2D8H9D() {
         //given
-        String  whiteString  = "2H 3C 4D 6S 7C";
-        String  blackString= "5S 6C 2D 8H 9D";
+        String whiteString = "2H 3C 4D 6S 7C";
+        String blackString = "5S 6C 2D 8H 9D";
         String result;
-        String expect="Black win";
+        String expect = "Black win";
         //when
-        result = pokerGame.play(whiteString,blackString);
+        result = pokerGame.play(whiteString, blackString);
         //then
-        Assertions.assertEquals(expect,result);
+        Assertions.assertEquals(expect, result);
+    }
+
+    @Test
+    void should_return_black_win_when_play_poker_given_3H4C5D6H7C_5C6C7D8S9H() {
+        //given
+        String whiteString = "3H 4C 5D 6H 7C";
+        String blackString = "5C 6C 7D 8S 9H";
+        String result;
+        String expect = "Black win";
+        //when
+        result = pokerGame.play(whiteString, blackString);
+        //then
+        Assertions.assertEquals(expect, result);
     }
 
 }
